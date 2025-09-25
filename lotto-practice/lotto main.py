642ff5c8c19df00ec18ea9658d4d8e6f78cee9a9
ainia0630 <ainia0630@gmail.com>
2025-09-25 14:27:20 +0900

    new lotto

--- lotto-practice/lotto main.py
+++ lotto-practice/lotto main.py
@@ -4,3 +4,25 @@ for i in range(5):
     lotto = random.sample(list(range(1,46)),6)
     lotto.sort()
     print(lotto)
+
+
+import tkinter
+import tkinter.font
+import random
+
+lotto_num = range(1,46)
+
+def buttonClick():
+    print(random.sample(lotto_num,6))
+
+window=tkinter.Tk()
+window.title("lotto")
+window.geometry("400x200")
+window.resizable(False, False)
+
+button = tkinter.Button(window, overrelief="solid",
+text="번호확인", width=15, command=buttonClick, repeatdelay=1000, repeatinterval=100)
+button.pack()
+
+window.mainloop()
+
